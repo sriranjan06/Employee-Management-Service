7ce79f0b6677fc1efd6861eecba7e76a2e6e348e
Sriranjan <sriranjan.s@brillio.com>
2024-03-28 23:45:17 -0700

    Added some ui files

---
+++ employee-management-system-ui/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import AddEmployee from "./components/AddEmployee";
+import Navbar from "./components/Navbar";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route index element={<EmployeeList />}></Route>
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />}></Route>
+          <Route path="/addEmployee" element={<AddEmployee />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
